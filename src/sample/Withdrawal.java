c181e03ffd6ff80f97e700667d34aa950362797c
carolinabado <cmbadocortes8401@eagle.fgcu.edu>
2019-09-13 16:12:36 -0400

    First commit

---
+++ src/sample/Withdrawal.java
@@ -0,0 +1,15 @@
+package sample;
+
+//this class represents an ATM withdrawal transaction
+public class Withdrawal {
+
+    //attributes
+    private int accountNumber; //the acct to withdraw money from
+    private double amount; //amt to withdraw
+
+    //references to associated objects
+    private Screen screen; //ATM's screen
+    private Keypad keypad; //ATM's keypad
+    //no argument constructor
+    public Withdrawal(){ }
+}
